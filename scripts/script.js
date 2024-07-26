1d23e7d30aefe4d7fc594d4d3f7bb1381663dde1
mohammad reza <mrghkh12@gmail.com>
2024-07-26 16:27:53 +0330

    feat(script.js): dynamic resaize elem left position

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,12 @@ const $ = document
  let imgBox = $.querySelector('.img-box')
  let imgWrapper = $.querySelector('.img-wrapper')
  let mainImg = $.querySelector('.main-img')
+ let resizeLine = $.querySelector('.resizeLine')
 
  mainImg.style.width = imgBox.offsetWidth + 'px'
 
 
  imgBox.addEventListener('mousemove', e => {
     imgWrapper.style.width =  (e.pageX - imgBox.offsetLeft) + 'px'
+    resizeLine.style.left = (e.pageX - imgBox.offsetLeft) + 'px'
  })
