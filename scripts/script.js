55a6279af8205f7d6b311682da79ec006516a4d9
mohammad reza <mrghkh12@gmail.com>
2024-07-26 16:14:17 +0330

    feat(script.js): set move event for change img to transparent bg

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,9 @@ const $ = document
  let imgWrapper = $.querySelector('.img-wrapper')
  let mainImg = $.querySelector('.main-img')
 
+ mainImg.style.width = imgBox.offsetWidth + 'px'
+
+
+ imgBox.addEventListener('mousemove', e => {
+    imgWrapper.style.width =  (e.pageX - imgBox.offsetLeft) + 'px'
+ })
