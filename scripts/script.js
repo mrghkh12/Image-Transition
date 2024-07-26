d6b3f0a0afb6116f1a5e1dd577eb65d51c8e16cb
mohammad reza <mrghkh12@gmail.com>
2024-07-26 16:38:01 +0330

    style: clean codes

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,17 @@
-const $ = document
+const $ = document;
 
- let imgBox = $.querySelector('.img-box')
- let imgWrapper = $.querySelector('.img-wrapper')
- let mainImg = $.querySelector('.main-img')
- let resizeLine = $.querySelector('.resizeLine')
+let imgBox = $.querySelector(".img-box");
+let imgWrapper = $.querySelector(".img-wrapper");
+let mainImg = $.querySelector(".main-img");
+let resizeLine = $.querySelector(".resizeLine");
 
- mainImg.style.width = imgBox.offsetWidth + 'px'
+mainImg.style.width = imgBox.offsetWidth + "px";
 
-
- imgBox.addEventListener('mousemove', e => {
-    imgWrapper.style.width =  (e.pageX - imgBox.offsetLeft) + 'px'
-    resizeLine.style.left = (e.pageX - imgBox.offsetLeft) + 'px'
- })
- imgBox.addEventListener('touchmove', e => {
-    imgWrapper.style.width =  (e.targetTouches[0].pageX - imgBox.offsetLeft) + 'px'
-    resizeLine.style.left = (e.targetTouches[0].pageX - imgBox.offsetLeft) + 'px'
- })
+imgBox.addEventListener("mousemove", (e) => {
+  imgWrapper.style.width = e.pageX - imgBox.offsetLeft + "px";
+  resizeLine.style.left = e.pageX - imgBox.offsetLeft + "px";
+});
+imgBox.addEventListener("touchmove", (e) => {
+  imgWrapper.style.width = e.targetTouches[0].pageX - imgBox.offsetLeft + "px";
+  resizeLine.style.left = e.targetTouches[0].pageX - imgBox.offsetLeft + "px";
+});
